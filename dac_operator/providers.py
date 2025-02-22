4369e836d1c8ccddd044080c2f47418607fba655
Morten Hauge <morten.hauge97@gmail.com>
2025-02-22 18:07:30 +0100

    feat: Azure configuration is now loaded from a ConfigMap in the customer namespace, rather than statically from the environment

--- dac_operator/providers.py
+++ dac_operator/providers.py
@@ -7,18 +7,27 @@ from dac_operator.microsoft_sentinel import (
 settings = get_settings()
 
 
-def get_microsoft_sentinel_repository():
+def get_microsoft_sentinel_repository(
+    tenant_id: str, subscription_id: str, resource_group_id: str, workspace_id: str
+):
     return microsoft_sentinel_repository.MicrosoftSentinelRepository(
-        tenant_id=settings.azure_tenant_id,
+        tenant_id=tenant_id,
         client_id=settings.client_id,
         client_secret=settings.client_secret,
-        subscription_id=settings.azure_subscription_id,
-        resource_group_id=settings.resource_group_id,
-        workspace_id=settings.azure_workspace_id,
+        subscription_id=subscription_id,
+        resource_group_id=resource_group_id,
+        workspace_id=workspace_id,
     )
 
 
-def get_microsoft_sentinel_service():
+def get_microsoft_sentinel_service(
+    tenant_id: str, subscription_id: str, resource_group_id: str, workspace_id: str
+):
     return microsoft_sentinel_service.MicrosoftSentinelService(
-        repository=get_microsoft_sentinel_repository()
+        repository=get_microsoft_sentinel_repository(
+            tenant_id=tenant_id,
+            resource_group_id=resource_group_id,
+            workspace_id=workspace_id,
+            subscription_id=subscription_id,
+        )
     )
