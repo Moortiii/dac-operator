fda74e0c3b55353f2fc503b907239562120f1904
Morten Hauge <morten.hauge97@gmail.com>
2025-03-03 15:06:20 +0100

    chore: Export both CRD schema and JSON-schema in preparation for Validating Admission Hook

--- rust/src/crds/microsoft_sentinel_macro.rs
+++ rust/src/crds/microsoft_sentinel_macro.rs
@@ -1,6 +1,6 @@
 use kube::core::CustomResourceExt;
 use kube_derive::CustomResource;
-use schemars::JsonSchema;
+use schemars::{schema_for, JsonSchema};
 use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::Write;
@@ -31,8 +31,16 @@ pub fn write_crd() -> std::io::Result<()> {
     // Write MicrosoftSentinelMacro CRD
     let filename = "MicrosoftSentinelMacro";
     let crd_yaml = serde_yaml::to_string(&MicrosoftSentinelMacro::crd()).unwrap();
-    let mut file = File::create(format!("./generated/{}.yaml", filename)).unwrap();
+    let mut file = File::create(format!("./generated/crds/{}.yaml", filename)).unwrap();
     file.write_all(crd_yaml.as_bytes()).unwrap();
-    println!("{filename} written to {filename}.yaml");
+    println!("{filename} CRD-schema written to {filename}.yaml");
+
+    // Write MicrosoftSentinelMacro Jsonschema
+    let filename = "MicrosoftSentinelMacro";
+    let schema = schema_for!(MicrosoftSentinelMacroSpec);
+    let crd_json = serde_json::to_string_pretty(&schema).unwrap();
+    let mut file = File::create(format!("./generated/jsonschema/{}.json", filename)).unwrap();
+    file.write_all(crd_json.as_bytes()).unwrap();
+    println!("{filename} JSON-schema written to {filename}.json");
     Ok(())
 }
