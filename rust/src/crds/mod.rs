7eb2fa45b7f727b280016625e255d34717647e0a
Morten Hauge <morten.hauge97@gmail.com>
2025-05-30 13:34:50 +0200

    chore: Create example CRD for Splunk

--- rust/src/crds/mod.rs
+++ rust/src/crds/mod.rs
@@ -2,3 +2,4 @@ pub mod microsoft_sentinel_analytic_rule;
 pub mod microsoft_sentinel_automation_rule;
 pub mod microsoft_sentinel_macro;
 pub mod microsoft_sentinel_workbook;
+pub mod splunk_detection_rule;
